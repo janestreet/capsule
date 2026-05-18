d9034125f8c7574729b0d49cee7e278d45985502
public-release <as-public-release@janestreet.com>
2026-05-18 12:36:23 +0100

    v0.18~preview.130.100+614

--- src/capsule_extended_intf.ml
+++ src/capsule_extended_intf.ml
@@ -53,89 +53,93 @@ module type Capsule = sig @@ portable
     (** Retrieve the value in a capsule directly. Likely only useful if the capsule has
         already been [map]'d, as capsules do not usually contain portable values. *)
     val get_id : ('a : value mod portable) 'k. ('a, 'k) t @ l -> 'a @ contended l]
+
+    [%%template:
+      val idx : ('a, 'k) t @ l -> ('a, 'b) idx_imm -> ('b, 'k) t @ l
+      [@@ocaml.doc
+        {| [idx t i] is a pointer to the value at [i] in [t].
+
+          See {!Ox.Idx} for more information on indexes. |}]
+      [@@mode l = (global, local)]]
   end
 
   module Isolated : sig
-    type%template ('a, 'k) inner =
-      #{ data : ('a, 'k) Data.t @@ u
-       ; key : 'k Capsule.Key.t
-       }
-    [@@modality.explicit u = (unique, aliased)]
+    (** The underlying representation of both [Frozen.t] and [Owned.t]. *)
+    module Repr : sig
+      type ('a, 'k) inner =
+        #{ data : ('a, 'k) Data.t
+         ; key : 'k Capsule.Key.t
+         }
 
-    type%template ('a, 'k) inner = (('a, 'k) inner[@modality.explicit aliased])
+      type 'a t = P : ('a, 'k) inner -> 'a t [@@unboxed]
+    end
+  end
 
-    [%%template:
-    [@@@mode.default u = (unique, aliased)]
+  module Frozen : sig
+    (** A frozen value in its own capsule. Nobody may write to the contained ['a], so
+        everyone may read from it.
 
-    (** A value isolated within its own capsule.
+        This type is useful for locally initializing some mutable data structure, then
+        permanently freezing it to give read access to multiple threads. *)
+    type 'a
+         t :
+         value
+         mod contended forkable many portable unyielding
+         with 'a portable @@ contended portable
 
-        A primary use-case for this type is to use aliasing as a proxy for contention.
-        [unique] access to an ['a Capsule.Isolated.t] allows [uncontended] access to the
-        underlying ['a]. [aliased] access to an ['a Capsule.Isolated.t] allows [shared]
-        access to the underlying ['a].
+    val to_repr : 'a t -> 'a portable Isolated.Repr.t
+    val of_repr : 'a portable Isolated.Repr.t -> 'a t
 
-        This type is templated over the uniqueness of the underlying ['a], defaulting to
-        the [aliased] mode. So if you have a ['a Capsule.Isolated.t @ unique], you can
-        only access the inner ['a @ aliased], but if you have an
-        [('a Capsule.Isolated.t[@mode unique]) @ unique], you can access the inner
-        ['a @ unique] *)
-    type 'a t = P : (('a, 'k) inner[@mode u]) -> ('a t[@mode u]) [@@unboxed]
-
-    (** A boxed representation of {{!t} an isolated value}. *)
-    type 'a boxed : value mod contended portable
-
-    val box : ('a t[@mode u]) @ unique -> ('a boxed[@mode u]) @ unique
-    val unbox : ('a boxed[@mode u]) @ unique -> ('a t[@mode u]) @ unique
-    val box_aliased : ('a t[@mode u]) -> ('a boxed[@mode u])
-    val unbox_aliased : ('a boxed[@mode u]) -> ('a t[@mode u])
-
-    (** [create f] runs [f] within a fresh capsule, and creates a [Capsule.Isolated.t]
+    (** [create f] runs [f] within a fresh capsule, and creates a [Capsule.Frozen.t]
         containing the result. *)
-    val create : (unit -> 'a @ u) @ local once portable -> ('a t[@mode u]) @ unique
+    val create : (unit -> 'a @ portable) @ local once portable -> 'a t
 
-    (** [with_shared t ~f] takes an [aliased] isolated capsule [t], calls [f] with shared
-        access to its value, and returns the result of [f]. *)
-    val with_shared
-      : ('a : value mod portable) 'b.
-      ('a t[@mode u])
-      -> f:('a @ shared -> 'b @ contended portable) @ local once portable
-      -> 'b @ contended portable
+    (** [unwrap t] takes a frozen capsule [t] and returns the underlying value at
+        [shared]. *)
+    val unwrap : 'a t -> 'a @ portable shared
+  end
 
-    [@@@mode.default l = (global, local)]
+  module Owned : sig
+    (** A capsule that uses [unique]ness tracking to enable different threads to gain
+        [uncontended] access to its contents at different times without need for runtime
+        synchronization. *)
+    type 'a
+         t :
+         value
+         mod contended forkable many portable unyielding
+         with 'a @@ contended portable
 
-    (** [unwrap t ~f] takes a [unique] isolated capsule [t] and returns the underlying
+    val to_repr : 'a t @ unique -> 'a Isolated.Repr.t @ unique
+    val of_repr : 'a Isolated.Repr.t @ unique -> 'a t @ unique
+
+    (** [create f] runs [f] within a fresh capsule, and creates a [Capsule.Owned.t]
+        containing the result. *)
+    val create : (unit -> 'a @ unique) @ local once portable -> 'a t @ unique
+
+    (** [freeze t] takes an [aliased] owned capsule (which can no longer be written to
+        since it isn't [unique]) and converts it to a {!Frozen.t}. *)
+    val freeze : ('a : value mod portable). 'a t -> 'a Frozen.t
+
+    (** [unwrap t] consumes a [unique] isolated capsule [t] and returns the underlying
         value, merging the capsule with the current capsule. *)
-    val unwrap : ('a t[@mode u]) @ l unique -> 'a @ l u
-
-    (** [unwrap_shared t ~f] takes an [aliased] isolated capsule [t] and returns the
-        underlying value at [shared]. *)
-    val unwrap_shared : ('a : value mod portable). ('a t[@mode u]) @ l -> 'a @ l shared]
-
-    (*_ Note that the following functions rely on the fact that the inner ['a] is aliased,
-        so only work on the version of [t] with aliased data. *)
+    val unwrap : 'a t @ unique -> 'a @ unique
 
     (** Project out a contended reference to the underlying value from a unique [t],
         returning the unique [t] back alongside the alias to the underlying value. *)
-    val get_id
-      : ('a : value mod portable).
-      'a t @ unique -> #('a t * 'a Modes.Aliased.t) @ contended unique
+    val get_contended
+      : ('a : value mod aliased portable).
+      'a t @ unique -> #('a t * 'a) @ contended unique
 
-    (** [with_unique t ~f] takes a [unique] isolated capsule [t], calls [f] with its
-        value, and returns a tuple of the unique isolated capsule and the result of [f]. *)
-    val with_unique
-      : 'a ('b : value mod contended portable).
+    (** [with_ t ~f] takes a [unique] isolated capsule [t], calls [f] with its value, and
+        returns a tuple of the unique isolated capsule and the result of [f]. *)
+    val with_
+      : ('a : value mod aliased) ('b : value mod contended portable).
       'a t @ unique
-      -> f:('a -> 'b) @ local once portable
-      -> #('a t * 'b Modes.Aliased.t) @ unique
-
-    (** Like [with_unique], but with the most general mode annotations. *)
-    val with_unique_gen
-      :  'a t @ unique
-      -> f:('a -> 'b @ contended portable unique) @ local once portable
-      -> #('a t * 'b) @ contended portable unique
+      -> f:('a -> 'b @ unique) @ local once portable
+      -> #('a t * 'b) @ unique
   end
 
-  module Guard : sig
+  module Scoped : sig
     type ('a, 'k) inner =
       #{ data : ('a, 'k) Data.t @@ global
        ; password : 'k Capsule.Password.t
@@ -143,12 +147,12 @@ module type Capsule = sig @@ portable
 
     (** An encapsulated value accessible for the duration of the current region.
 
-        A value of type ['a Guard.t] provides [uncontended] access to the underlying ['a]
+        A value of type ['a Scoped.t] provides [uncontended] access to the underlying ['a]
         over a local scope. *)
     type 'a t = P : ('a, 'k) inner -> 'a t [@@unboxed]
 
-    (** [with_ a ~f] calls [f] with a [local] {!Guard.t} representing local access to [a],
-        which lives in the current capsule. *)
+    (** [with_ a ~f] calls [f] with a [local] {!Scoped.t} representing local access to
+        [a], which lives in the current capsule. *)
     val with_ : 'a -> f:('a t @ local -> 'b) @ local once -> 'b
 
     (** [get t ~f] computes a value using the data accessible via [t]. *)
@@ -171,9 +175,9 @@ module type Capsule = sig @@ portable
 
       (** An encapsulated value that may be read for the duration of the current region.
 
-          A value of type ['a Shared.t] provides [shared] access to the underlying ['a]
-          over a local scope. A [forkable] ['a Shared.t] can be captured by functions that
-          run on other domains. *)
+          A value of type ['a Scoped.Shared.t] provides [shared] access to the underlying
+          ['a] over a local scope. A [forkable] ['a Scoped.Shared.t] can be captured by
+          functions that run from other capsules. *)
       type 'a t = P : ('a, 'k) inner -> 'a t [@@unboxed]
 
       (** [with_ a ~f] calls [f] with a [local forkable] {!Shared.t} representing local
@@ -202,8 +206,8 @@ module type Capsule = sig @@ portable
         -> 'b t @ local
 
       module Uncontended : sig
-        (** Like ['a Shared.t], but allows read-only computations to return an uncontended
-            result in the current capsule. *)
+        (** Like ['a Scoped.Shared.t], but allows read-only computations to return an
+            uncontended result in the current capsule. *)
         type ('a, 'k) t = ('a, 'k) inner =
           #{ data : ('a shared, 'k) Data.t @@ global
            ; password : 'k Capsule.Password.Shared.t
@@ -212,8 +216,9 @@ module type Capsule = sig @@ portable
         type ('a, 'b) f =
           { f : 'k. ('a, 'k) t @ forkable local -> ('b, 'k) Capsule.Data.Shared.t }
 
-        (** [with_ a ~f] calls [f] with a [local forkable] {!Uncontended.t} representing
-            local read-only access to [a], which is readable in the current capsule.
+        (** [with_ a ~f] calls [f] with a [local forkable] {!Scoped.Shared.Uncontended.t}
+            representing local read-only access to [a], which is readable in the current
+            capsule.
 
             The result of [f] is a [Capsule.Data.Shared.t], which can be unwrapped in the
             current capsule. *)
@@ -237,9 +242,10 @@ module type Capsule = sig @@ portable
   end
 
   module (Initial @@ nonportable) : sig
-    (** The initial capsule, i.e. the implicit capsule associated with the initial domain.
-        This is the capsule in which library top-levels run, and so [nonportable]
-        top-level functions are allowed to access it. *)
+    (** The initial capsule is the implicit capsule associated with the OCaml top level.
+        Since this is the capsule in which library top-levels run, any [nonportable]
+        top-level function belongs to the initial capsule, and hence is allowed to access
+        it. *)
 
     (** The brand for the initial capsule. *)
     type k = Capsule.initial
@@ -255,11 +261,11 @@ module type Capsule = sig @@ portable
       [@@@mode.default l = (global, local)]
 
       (** Store a value in a [Capsule.Data.t] for the initial capsule. This function is
-          [nonportable], requiring it to be run from the initial domain. *)
+          [nonportable], requiring it to only be run from the initial capsule. *)
       val wrap : 'a @ l -> 'a t @ l
 
       (** Extract a value from a [Capsule.Data.t] for the initial capsule. This function
-          is [nonportable], requiring it to be run from the initial domain. *)
+          is [nonportable], requiring it to only be run from the initial capsule. *)
       val unwrap : 'a t @ l -> 'a @ l]
     end
   end
