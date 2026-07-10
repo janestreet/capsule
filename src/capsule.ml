24c12e76eb299b962eefc768d6914dd9de195bbb
public-release <as-public-release@janestreet.com>
2026-07-10 17:38:58 +0100

    v0.18~preview.130.106+341

--- src/capsule.ml
+++ src/capsule.ml
@@ -1,3 +1,9 @@
+open! Base
+
+(** Capsules
+
+    Note that this module is reexported by [Core] *)
+
 module Blocking_sync = Capsule_blocking_sync
-module Expert = Capsule_expert
-include Capsule_extended
+module Prim = Capsule_prim
+include Prim.Extended
